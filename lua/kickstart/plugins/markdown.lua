1fd5465efc8212dbae7b35918cf6eb47d79ee6a3
Rajive Joshi <rajive.joshi@gmail.com>
2025-07-11 15:20:21 -0700

    markdown: Add improved markdown rendering

---
+++ lua/kickstart/plugins/markdown.lua
@@ -0,0 +1,18 @@
+-- improve viewing Markdown files in Neovim
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
+  },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    file_types = { 'markdown' },
+
+    completions = {
+      lsp = { enabled = true },
+      blink = { enabled = true },
+    },
+  },
+}
